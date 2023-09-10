f65a8df0380e9f1c1747490b49e5f44e351d2232
Alex Sharov <AskAlexSharov@gmail.com>
2023-09-10 15:46:27 +0700

    Downloader: don't fail when see unusual file, skip it (backward/forward compatibility) #1117

--- downloader/snaptype/files.go
+++ downloader/snaptype/files.go
@@ -111,16 +111,15 @@ func IsCorrectHistoryFileName(name string) bool {
 	return len(parts) == 3
 }
 
-func ParseFileName(dir, fileName string) (res FileInfo, err error) {
+func ParseFileName(dir, fileName string) (res FileInfo, ok bool) {
 	ext := filepath.Ext(fileName)
 	onlyName := fileName[:len(fileName)-len(ext)]
 	parts := strings.Split(onlyName, "-")
 	if len(parts) < 4 {
-		return res, fmt.Errorf("expected format: v1-001500-002000-bodies.seg got: %s. %w", fileName, ErrInvalidFileName)
+		return res, ok
 	}
-	if parts[0] != "v1" {
-		return res, fmt.Errorf("version: %s. %w", parts[0], ErrInvalidFileName)
-	}
+	version := parts[0]
+	_ = version
 	from, err := strconv.ParseUint(parts[1], 10, 64)
 	if err != nil {
 		return
@@ -129,22 +128,11 @@ func ParseFileName(dir, fileName string) (res FileInfo, err error) {
 	if err != nil {
 		return
 	}
-	var snapshotType Type
 	ft, ok := ParseFileType(parts[3])
 	if !ok {
-		return res, fmt.Errorf("unexpected snapshot suffix: %s,%w", parts[2], ErrInvalidFileName)
+		return res, ok
 	}
-	switch ft {
-	case Headers:
-		snapshotType = Headers
-	case Bodies:
-		snapshotType = Bodies
-	case Transactions:
-		snapshotType = Transactions
-	default:
-		return res, fmt.Errorf("unexpected snapshot suffix: %s,%w", parts[2], ErrInvalidFileName)
-	}
-	return FileInfo{From: from * 1_000, To: to * 1_000, Path: filepath.Join(dir, fileName), T: snapshotType, Ext: ext}, nil
+	return FileInfo{From: from * 1_000, To: to * 1_000, Path: filepath.Join(dir, fileName), T: ft, Ext: ext}, ok
 }
 
 const Erigon3SeedableSteps = 32
@@ -203,12 +191,9 @@ func ParseDir(dir string) (res []FileInfo, err error) {
 			continue
 		}
 
-		meta, err := ParseFileName(dir, f.Name())
-		if err != nil {
-			if errors.Is(err, ErrInvalidFileName) {
-				continue
-			}
-			return nil, err
+		meta, ok := ParseFileName(dir, f.Name())
+		if !ok {
+			continue
 		}
 		res = append(res, meta)
 	}
