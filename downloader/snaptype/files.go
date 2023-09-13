7b10675bac417b2d48dfac9f763a1b15916f00a8
Alex Sharov <AskAlexSharov@gmail.com>
2023-09-13 15:21:38 +0700

    downloader: support --chain parameter (#1124)

--- downloader/snaptype/files.go
+++ downloader/snaptype/files.go
@@ -17,8 +17,10 @@
 package snaptype
 
 import (
+	"encoding/hex"
 	"errors"
 	"fmt"
+	"github.com/anacrolix/torrent/metainfo"
 	"os"
 	"path/filepath"
 	"strconv"
@@ -214,4 +216,13 @@ func ParseDir(dir string) (res []FileInfo, err error) {
 	})
 
 	return res, nil
+}
+
+func Hex2InfoHash(in string) (infoHash metainfo.Hash) {
+	inHex, err := hex.DecodeString(in)
+	if err != nil {
+		panic(err)
+	}
+	copy(infoHash[:], inHex)
+	return infoHash
 }
