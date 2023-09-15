5eeb7834bfdb76230872c70613cebcf1fded216f
blxdyx <eric.z@nodereal.io>
2023-09-15 11:48:15 +0800

    merge

--- chain/consensus.go
+++ chain/consensus.go
@@ -5,6 +5,7 @@ type ConsensusName string
 const (
 	AuRaConsensus   ConsensusName = "aura"
 	EtHashConsensus ConsensusName = "ethash"
+	ParliaConsensus ConsensusName = "parlia"
 	CliqueConsensus ConsensusName = "clique"
 	BorConsensus    ConsensusName = "bor"
 )
